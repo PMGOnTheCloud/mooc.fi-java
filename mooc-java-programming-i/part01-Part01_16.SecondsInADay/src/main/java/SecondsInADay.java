74108e8c97b056db1baea0a260691b4ed0813cd1
PMGOnTheCloud <pablomartingranara@gmail.com>
2023-01-20 00:18:51 +0100

    Exercise 01-01_16 done

--- mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ mooc-java-programming-i/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,6 +7,8 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("How many days would you like to convert to seconds?");
+        int numOfDays = Integer.valueOf(scanner.nextLine());
+        System.out.println(numOfDays * 24 * 60 * 60);
     }
 }
