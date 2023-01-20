f5e1b0bb8ac49190238d0a917a796952147d8ddc
PMGOnTheCloud <pablomartingranara@gmail.com>
2023-01-20 20:24:08 +0100

    'Recurring problems and patterns to solve them' exercises done

--- mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,7 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int numberToBeSquared = scanner.nextInt();
+        System.out.println(numberToBeSquared * numberToBeSquared);
     }
 }
--- mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,13 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        
+        int sumOfNumbers = firstNumber + secondNumber;
+        
+        if (!(sumOfNumbers < 0)) {
+            System.out.println(Math.sqrt(sumOfNumbers));
+        }
     }
 }
