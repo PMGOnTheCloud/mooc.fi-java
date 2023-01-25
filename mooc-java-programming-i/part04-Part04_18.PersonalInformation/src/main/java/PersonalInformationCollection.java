d6acabffd020fb52953ff0b247e2793d8ddb59cb
PMGOnTheCloud <pablomartingranara@gmail.com>
2023-01-25 19:31:52 +0100

    'Objects in a list' exercises done

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,27 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            System.out.println("First name:");
+            String firstName = scanner.nextLine();
+            
+            if (firstName.equals("")) {
+                break;
+            }
+            
+            System.out.println("Last name:");
+            String lastName = scanner.nextLine();
+            System.out.println("Identification number:");
+            String idNumber = scanner.nextLine();
+            
+            PersonalInformation personalInfo = new PersonalInformation(firstName, lastName, idNumber);
+            
+            infoCollection.add(personalInfo);
+        }
 
+        for(PersonalInformation personalInfo: infoCollection) {
+            System.out.println(personalInfo.getFirstName() + " " + personalInfo.getLastName());
+        }
     }
 }
--- mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,37 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> library = new ArrayList<>();
+        
+        while(true) {
+            System.out.println("Title:");
+            String title = scanner.nextLine();
+            
+            if (title.isEmpty()) {
+                break;
+            }
+            
+            System.out.println("Pages:");
+            int pages = Integer.parseInt(scanner.nextLine());
+            System.out.println("Publication year:");
+            int publicationYear = Integer.parseInt(scanner.nextLine());
+            
+            Book book = new Book(title, pages, publicationYear);
+            
+            library.add(book);
+            
+        }
+        
+        System.out.println("What information will be printed?");
+        String wantedInfoPrint = scanner.nextLine();
+        
+        library.forEach((book) -> {
+            if (wantedInfoPrint.equals("name")) {
+                System.out.println(book.getTitle());
+            } else if (wantedInfoPrint.equals("everything")) {
+                System.out.println(book);
+            }
+        });
     }
 }
